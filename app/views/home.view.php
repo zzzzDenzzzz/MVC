f2afd1a77c5ada51c4400477cb84ab78fd7dc2ca
zzzzDenzzzz <12den24@gmail.com>
2024-05-08 08:32:43 +0300

    fix

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -4,106 +4,72 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <meta name="description" content="">
     <meta name="author" content="">
-
     <title>Kool Form Pack | Coming Soon Page</title>
-
     <!-- CSS FILES -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
-
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,400;0,700;1,200&family=Unbounded:wght@400;700&display=swap"
         rel="stylesheet">
-
     <link href="<?= ROOT ?>/assets/css/bootstrap.min.css" rel="stylesheet">
-
     <link href="<?= ROOT ?>/assets/css/bootstrap-icons.css" rel="stylesheet">
-
     <link href="<?= ROOT ?>/assets/css/tooplate-kool-form-pack.css" rel="stylesheet">
-
-    <!--
-
-Tooplate 2136 Kool Form Pack
-
-https://www.tooplate.com/view/2136-kool-form-pack
-
-Bootstrap 5 Form Pack Template
-
--->
 </head>
 
 <body>
-
     <main>
-
         <header class="site-header">
             <div class="container">
                 <div class="row justify-content-between">
-
                     <div class="col-lg-12 col-12 d-flex align-items-center">
                         <a class="site-header-text d-flex justify-content-center align-items-center me-auto"
                             href="<?= ROOT ?>">
                             <i class="bi-box"></i>
-
                             <span>
                                 Kool Form Pack
                             </span>
                         </a>
-
                         <ul class="social-icon d-flex justify-content-center align-items-center mx-auto">
                             <span class="text-white me-4 d-none d-lg-block">Stay connected</span>
-
                             <li class="social-icon-item">
                                 <a href="#" class="social-icon-link bi-instagram"></a>
                             </li>
-
                             <li class="social-icon-item">
                                 <a href="#" class="social-icon-link bi-twitter"></a>
                             </li>
-
                             <li class="social-icon-item">
                                 <a href="#" class="social-icon-link bi-whatsapp"></a>
                             </li>
                         </ul>
-
                         <div>
                             <a href="#" class="custom-btn custom-border-btn btn" data-bs-toggle="modal"
                                 data-bs-target="#subscribeModal">Notify me
                                 <i class="bi-arrow-right ms-2"></i>
                             </a>
                         </div>
-
                         <a class="bi-list offcanvas-icon" data-bs-toggle="offcanvas" href="#offcanvasMenu" role="button"
-                            aria-controls="offcanvasMenu"></a>
-
+                            aria-controls="offcanvasMenu">
+                        </a>
                     </div>
-
                 </div>
             </div>
         </header>
-
-
         <div class="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="offcanvasMenu"
             aria-labelledby="offcanvasMenuLabel">
             <div class="offcanvas-header">
                 <button type="button" class="btn-close ms-auto" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
-
             <div class="offcanvas-body d-flex flex-column justify-content-center align-items-center">
                 <nav>
                     <ul>
                         <li>
                             <a href="<?= ROOT ?>/login">Login Form</a>
                         </li>
-
                         <li>
                             <a href="register.html">Create an account</a>
                         </li>
-
                         <li>
                             <a href="contact.html">Contact Form</a>
                         </li>
@@ -111,67 +77,53 @@ Bootstrap 5 Form Pack Template
                 </nav>
             </div>
         </div>
-
-
         <!-- Modal -->
         <div class="modal fade" id="subscribeModal" tabindex="-1" aria-labelledby="subscribeModalLabel"
             aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
                 <div class="modal-content">
                     <div class="modal-header">
-
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-
                     <div class="modal-body">
                         <form action="#" method="get" class="custom-form mt-lg-4 mt-2" role="form">
                             <h2 class="modal-title" id="subscribeModalLabel">Stay up to date</h2>
-
                             <input type="email" name="email" id="email" pattern="[^ @]*@[^ @]*" class="form-control"
                                 placeholder="your@email.com" required="">
-
                             <button type="submit" class="form-control">Notify</button>
                         </form>
                     </div>
-
                     <div class="modal-footer justify-content-center">
                         <p>By signing up, you agree to our Privacy Notice</p>
                     </div>
                 </div>
             </div>
         </div>
-
-
         <section class="hero-section d-flex justify-content-center align-items-center" id="section_1">
             <div class="container">
                 <div class="row">
-
                     <div class="col-lg-6 col-12 mx-auto">
                         <small>our website is under construction.
                             <a rel="nofollow" target="_blank" href="<?= ROOT ?>/assets/videos/planet-earth.mp4">Earth
-                                video</a></small>
-                        <!-- please set the date time in the init.js file -->
-
+                                video
+                            </a>
+                        </small>
                         <h1 class="text-white mt-2 mb-4 pb-2">
                             Stay tuned!
                         </h1>
-
                         <ul class="countdown d-flex flex-wrap align-items-center">
                             <li class="countdown-item d-flex flex-column justify-content-center align-items-center">
                                 <h2 class="countdown-title days">0</h2>
                                 <span class="countdown-text">Days</span>
                             </li>
-
                             <li class="countdown-item d-flex flex-column justify-content-center align-items-center">
                                 <h2 class="countdown-title hours">0</h2>
                                 <span class="countdown-text">Hours</span>
                             </li>
-
                             <li class="countdown-item d-flex flex-column justify-content-center align-items-center">
                                 <h2 class="countdown-title minutes">0</h2>
                                 <span class="countdown-text">Minutes</span>
                             </li>
-
                             <li class="countdown-item d-flex flex-column justify-content-center align-items-center">
                                 <h2 class="countdown-title seconds">0</h2>
                                 <span class="countdown-text">Seconds</span>
@@ -180,23 +132,19 @@ Bootstrap 5 Form Pack Template
                     </div>
                 </div>
             </div>
-
             <div class="video-wrap">
                 <video autoplay="" loop="" muted="" class="custom-video" poster="">
                     <source src="<?= ROOT ?>/assets/videos/video.mp4" type="video/mp4">
-
                     Your browser does not support the video tag.
                 </video>
             </div>
         </section>
     </main>
-
     <!-- JAVASCRIPT FILES -->
     <script src="<?= ROOT ?>/assets/js/jquery.min.js"></script>
     <script src="<?= ROOT ?>/assets/js/bootstrap.bundle.min.js"></script>
     <script src="<?= ROOT ?>/assets/js/countdown.js"></script>
     <script src="<?= ROOT ?>/assets/js/init.js"></script>
-
 </body>
 
 </html>
